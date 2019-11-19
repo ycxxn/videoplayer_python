007c4dedc03363af0fb1e49519b765099cf4a191
ycxxn <41862983+ycxxn@users.noreply.github.com>
2019-11-20 03:59:35 +0800

    Add files via upload

--- ver_2/videoplayer.py
+++ ver_2/videoplayer.py
@@ -4,31 +4,19 @@ class CVideoPlayer:
     def __init__(self):
         self.isPause = False
         self.isStop = True
-
-        # Mouse Event
-        self.drawing = False
-        self.mode = True 
-        self.ix,self.iy = -1,-1
+        self.tracker = cv2.TrackerBoosting_create()
         self.roi_list = []
-        #
+        self.ok = False
 
     def draw_(self,event,x,y,flags,param):
-
         if event == cv2.EVENT_LBUTTONDOWN :
-            # self.drawing = True
-            # if self.drawing == True:
             self.ix,self.iy = x,y
 
         elif event == cv2.EVENT_LBUTTONUP:
-            # self.drawing = False
-            # if self.drawing == True:
             cv2.rectangle(self.frame,(self.ix,self.iy),(x,y),(255,0,0),1)
-            # print(self.ix,self.iy,x,y)
-            # print("xmin:{xmin} ymin:{ymin} xmax:{xmax} ymax:{ymax}".format(xmin=self.ix,ymin=self.iy,xmax=x,ymax=y))
-            self.roi_list.append([self.ix,self.iy,x,y])
-                # print(self.roi_list)
-                # self.ix,self.iy=-1,-1
-            # self.drawing = False
+            self.roi_list.append([self.ix,self.iy,x-self.ix,y-self.iy])
+            self.ok = self.tracker.init(self.frame,(self.ix,self.iy,x-self.ix,y-self.iy))
+
         if event == cv2.EVENT_MBUTTONDOWN:
             self.roi_list = []
 
@@ -42,15 +30,23 @@ class CVideoPlayer:
 
     def CreateVideoPlayback(self,video_path = "video.mp4"):
         self.cap = cv2.VideoCapture(video_path)
-
+        i= 0
         while(1):
             if self.isPause == False:
                 ret, self.frame = self.cap.read()
-            if ret == False:
-                break
-            print(self.roi_list)
+
+            if self.ok:
+            # Tracking success
+                bbox = self.tracker.update(self.frame)
+                p1 = (int(bbox[0]), int(bbox[1]))
+                p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+                cv2.rectangle(self.frame, p1, p2, (255,0,0), 2, 1)
+                i+=1
+                print(i)
+
             cv2.imshow("frame",self.frame)
             cv2.setMouseCallback('frame',self.draw_)
+        
             if cv2.waitKey(24) & 0xFF == ord('q'):
                 break
          
